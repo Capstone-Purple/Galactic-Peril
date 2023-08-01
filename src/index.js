1ca4642735c657a391e7bea5312ddd169f4864f3
Joaquin Maguire <thejmags@gmail.com>
2023-08-01 13:21:57 -0700

    Runs express and allows you to import Phaser

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const Phaser = require('phaser');
+
+console.log('We are accessing the JS.');
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
