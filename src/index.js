360a697dbf8e3b48499bd3838048139c50ab1914
vhuang325 <vhuang325@gmail.com>
2023-08-02 18:42:13 -0400

    rendered character on screen

--- src/index.js
+++ src/index.js
@@ -17,10 +17,32 @@ var game = new Phaser.Game(config);
 
 function preload() {
   this.load.image("background", "/Starship-Map.png");
+  this.load.image("character", "/CharDown.png");
 }
 
 function create() {
-  this.add.image(525, 300, "background");
+  this.background = this.add.image(490, 250, "background");
+  this.character = this.add.image(
+    config.width / 2,
+    config.height / 2,
+    "character"
+  );
+  this.character.setScale(1.5);
 }
 
-function update() {}
+function update() {
+  moveCharacter(this.character, 1);
+}
+
+function moveCharacter(character, speed) {
+  character.y += speed;
+  if (character.y > config.height) {
+    resetCharacterPosition(character);
+  }
+}
+
+function resetCharacterPosition(character) {
+  character.y = 0;
+  // var randomX = Phaser.Math.Between(0, config.width);
+  // character.x = randomX;
+}
