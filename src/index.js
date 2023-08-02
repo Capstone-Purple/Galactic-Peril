1f5806d5b96676814499c9cecf44235d54b032cf
rob-rp <robert.pasols@yahoo.com>
2023-08-02 11:50:47 -0400

    Testing images and characters on my branch. Will BE deleted.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,65 @@
 const Phaser = require('phaser');
-
+// import Example1 from './Example1';
 console.log('We are accessing the JS.');
+let player;
+let player2;
+let cursors;
 
-var config = {
+
+let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+    physics: {
+        default: 'arcade'
+    },
+    scene: {preload: preload,
+            create: create,
+            update: update
     }
 };
 
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 
-function preload() {
+function preload () {
+    this.load.image('ship', '/images/Ship3_Bottom.png');
+    this.load.image('space', '/images/Background.png')
+    this.load.image('captain', '/images/CaptainMale.png')
+    this.load.image('alien', '/images/Aliens.png')
 }
-
+    
 function create() {
+    // this.add.image(800,600,'space')
+    this.add.image(400,300, 'ship')//this.image = this.add.image(400,300, 'player')
+
+    player = this.physics.add.sprite(400, 300, 'captain');
+    player2 = this.physics.add.sprite(600,300, 'alien')
+    player.setCollideWorldBounds(true)
+    cursors = this.input.keyboard.createCursorKeys();
+    this.cameras.main.setSize(1000,1000);
+    this.cameras.main.startFollow(player,false,0.1,0.1)
 }
 
 function update() {
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+    }
+    else
+    {
+        player.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown /*&& player.body.touching.down*/)
+    {
+        player.setVelocityY(-160);
+    }
+    else if(cursors.down.isDown)
+    {
+        player.setVelocityY(160);
+    }
 }
