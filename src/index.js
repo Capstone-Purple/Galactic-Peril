9a07f4383336118be6745a828a19ef5bceef3e5f
rob-rp <robert.pasols@yahoo.com>
2023-08-07 17:51:05 -0400

    Added in Enemy character with tracker and player collision

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const Phaser = require('phaser');
 const Player = require('./player.js').default;
+const Enemy = require('./enemy.js').default;
 import Example2 from './Example2';
 
 let player;
+let enemy;
 let platform;
 
 let config = {
@@ -31,6 +33,8 @@ function preload() {
     // this.load.image('door', '/images/ShipDoor.png');
     // this.load.image('background', '/Starship-Map.png');
     this.load.spritesheet('captain', '/images/YappinToTheCaptain.png', { frameWidth: 80, frameHeight: 80 });
+    this.load.spritesheet('alien', '/images/femaleCaptain.png', { frameWidth: 80, frameHeight: 80});
+
 }
 
 function create() {
@@ -44,7 +48,16 @@ function create() {
     wallsLayer.setCollisionByProperty({collides: true});
 
     player = new Player(this, 400, 300);
+    // player.sprite.setImmovable(true)
     this.physics.add.collider(player.sprite, wallsLayer);
+
+    enemy = new Enemy(this, 600, 300);
+    enemy.sprite.setImmovable(true)
+    this.physics.add.collider(enemy.sprite, wallsLayer);
+
+    this.physics.add.collider(player.sprite, enemy.sprite, function () {
+        player.sprite.setTint(0xff0000)
+    })
 
     // platform = this.physics.add.staticGroup();
     // let door = platform.create(160, 0, 'door').setAlpha(0);
@@ -55,4 +68,5 @@ function create() {
 
 function update() {
     player.update();
+    enemy.update(player);
 }
