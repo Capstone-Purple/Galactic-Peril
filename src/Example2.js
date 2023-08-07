bf9812be86d1159bb8f9879b4c2d566b66a948ea
Joaquin Maguire <thejmags@gmail.com>
2023-08-06 18:13:10 -0700

    Player module works in Example2

--- src/Example2.js
+++ src/Example2.js
@@ -1,88 +1,66 @@
 let player;
 let player2;
-let cursors;
 let cameras;
 let panel1, panel2;
 let platform;
 // let interaction1;
 const Phaser = require('phaser');
+const Player = require('./player.js').default;
 
 class Example2 extends Phaser.Scene {
     constructor() {
-        super({key:"Example2"});
+        super({ key: "Example2" });
     }
-    
-    preload () {
-    this.load.image('ship2', '/images/Starship-Map.png');
-    this.load.image('space', '/images/Background.png')
-    this.load.image('captain', '/images/CaptainMale.png')
-    this.load.image('alien', '/images/Aliens.png')
-    this.load.image('closedpanel', '/images/ClosedPanel.png')
-    this.load.image('openpanel', '/images/OpenPanel.png')
-    this.load.image('door', '/images/ShipDoor.png')
 
+    preload() {
+        this.load.image('ship2', '/images/Starship-Map.png');
+        this.load.image('space', '/images/Background.png')
+        this.load.image('captain', '/images/CaptainMale.png')
+        this.load.image('alien', '/images/Aliens.png')
+        this.load.image('closedpanel', '/images/ClosedPanel.png')
+        this.load.image('openpanel', '/images/OpenPanel.png')
+        this.load.image('door', '/images/ShipDoor.png')
+        this.load.spritesheet('captain', '/images/YappinToTheCaptain.png', { frameWidth: 80, frameHeight: 80 });
     }
-    
+
     create() {
         // this.add.image(800,600,'space')
-        this.add.image(400,500, 'ship2')//this.image = this.add.image(400,300, 'player')
+        this.add.image(400, 500, 'ship2')//this.image = this.add.image(400,300, 'player')
 
-        player = this.physics.add.sprite(300, 500, 'captain');
-        player2 = this.physics.add.sprite(600,500, 'alien')
-        player.setCollideWorldBounds(true)
-        cursors = this.input.keyboard.createCursorKeys();
-        this.cameras.main.setSize(1000,1000);
-        this.cameras.main.startFollow(player,false,0.1,0.1)
+        // player = this.physics.add.sprite(300, 500, 'captain');
+        player = new Player(this, 300, 500);
+        player2 = this.physics.add.sprite(600, 500, 'alien')
+        player.sprite.setCollideWorldBounds(true)
+        this.cameras.main.setSize(1000, 1000);
+        this.cameras.main.startFollow(player.sprite, false, 0.1, 0.1)
         platform = this.physics.add.staticGroup();
         panel1 = platform.create(220, 30, 'closedpanel');
         let panel3 = platform.create(300, 30, 'closedpanel');
         // let door = platform.create(150,20,'door');
 
         // let thatThis = this;
-        this.physics.add.collider(player,panel1, function() {
+        this.physics.add.collider(player.sprite, panel1, function () {
             this.input.keyboard.on('keyup-SPACE', (event) => {
                 console.log('event => ', event)
-                panel1.disableBody(true,true);
+                panel1.disableBody(true, true);
                 panel2 = platform.create(250, 30, 'openpanel');
-            },this)
-        },null,this);
+            }, this)
+        }, null, this);
 
-        this.physics.add.collider(player,panel3, function() {
+        this.physics.add.collider(player.sprite, panel3, function () {
             this.input.keyboard.on('keyup-SPACE', (event) => {
                 console.log('event => ', event)
-                panel1.disableBody(true,true);
+                panel1.disableBody(true, true);
                 panel2 = platform.create(250, 30, 'openpanel');
-            },this)
-        },null,this);
-               
+            }, this)
+        }, null, this);
+
     }
 
     update() {
-        if (cursors.left.isDown)
-        {
-            player.setVelocityX(-160);
-        }
-        else if (cursors.right.isDown)
-        {
-            player.setVelocityX(160);
-        }
-        else
-        {
-            player.setVelocityX(0);
-        }
-
-        if (cursors.up.isDown /*&& player.body.touching.down*/)
-        {
-            player.setVelocityY(-160);
-        }
-        else if(cursors.down.isDown)
-        {
-            player.setVelocityY(160);
-        }
+        player.update();
     }
-    
+
 }
-
-
 
 module.exports = Example2;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const Phaser = require('phaser');
+const Player = require('./player.js').default;
+
 import Example2 from './Example2';
 console.log('We are accessing the JS.');
 let player;
 let player2;
-let cursors;
 let platform;
-
 
 let config = {
     type: Phaser.AUTO,
@@ -37,65 +37,20 @@ function create() {
     // this.add.image(800,600,'space')
     this.add.image(400,300, 'ship')//this.image = this.add.image(400,300, 'player')
 
-    player = this.physics.add.sprite(400, 300, 'captain');
-    player2 = this.physics.add.sprite(600,300, 'alien')
-    player.setCollideWorldBounds(true)
+    // player = this.physics.add.sprite(400, 300, 'captain');
+    player = new Player(this, 400, 300);
+    // player2 = this.physics.add.sprite(600,300, 'alien')
+    player.sprite.setCollideWorldBounds(true)
 
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('captain', {start: 3, end: 5}),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('captain', {start: 6, end: 8}),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'up',
-        frames: this.anims.generateFrameNumbers('captain', {start: 9, end: 11}),
-        frameRate: 10,
-        repeeat: -1
-    });
-    this.anims.create({
-        key: 'down',
-        frames: this.anims.generateFrameNumbers('captain', {start: 0, end: 2}),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    cursors = this.input.keyboard.createCursorKeys();
     this.cameras.main.setSize(1000,1000);
-    this.cameras.main.startFollow(player,false,0.1,0.1)
+    this.cameras.main.startFollow(player.sprite,false,0.1,0.1)
     platform = this.physics.add.staticGroup();
     let door = platform.create(160,0,'door').setAlpha(0);
-    this.physics.add.collider(player, door, function() {
+    this.physics.add.collider(player.sprite, door, function() {
       this.scene.start('Example2')
     },null,this);
   }
 
 function update() {
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
-        player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown) {
-        player.setVelocityX(160);
-        player.anims.play('right', true);
-    }
-    else if (cursors.up.isDown) {
-        player.setVelocityY(-160);
-        player.anims.play('up', true);
-    }
-    else if (cursors.down.isDown) {
-        player.setVelocityY(160);
-        player.anims.play('down', true);
-    }
-    else {
-        player.anims.stop();
-        player.setVelocityX(0);
-        player.setVelocityY(0);
-    }
+    player.update();
 }
