74c317b1ba7d9c5bac6cd5d513a303f5840d7112
JoeLomuto <lomutoj@gmail.com>
2023-08-08 14:35:36 -0400

    starting inventory class

--- src/player.js
+++ src/player.js
@@ -1,5 +1,23 @@
 const Phaser = require("phaser");
 
+class Inventory {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = "rgba(255, 255, 255, 0.5)";
+    this.items = new Array();
+    this.columns = 4;
+  }
+
+  addItem(sprite) {
+    if (this.items.length == this.columns) {
+      
+    }
+  }
+}
+
 class HealthBar {
   constructor(scene, x, y) {
     this.bar = new Phaser.GameObjects.Graphics(scene);
