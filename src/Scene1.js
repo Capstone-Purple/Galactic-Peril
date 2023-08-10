df9a18d1f24e1419974627909582686505b6888a
rob-rp <robert.pasols@yahoo.com>
2023-08-09 21:43:36 -0400

    Updated Save/Load and added in Game Over functionality

--- src/Scene1.js
+++ src/Scene1.js
@@ -12,26 +12,27 @@ class Scene1 extends Phaser.Scene {
     }
     
     preload() {
+        // this.load.image('deadPlayer', '/images/redblob.png')
         this.load.image("floor-tiles", "tilesets/A5_SciFi.png");
-  this.load.image("wall-tiles", "tilesets/Ship2_Bottom.png");
-  this.load.image("machinary1-tiles", "tilesets/SciFi_Computers_1.png");
-  this.load.image("machinary2-tiles", "tilesets/SciFi_Computers_2.png");
-  this.load.image("screens-tiles", "tilesets/!$ViewScreen_7.png");
-  this.load.image("door-tiles", "/tilesets/!$Objects_1.png");
-  this.load.tilemapTiledJSON("starship", "tilesets/Starship-Map.json");
-  // this.load.image('ship', '/images/Ship3_Bottom.png');
-  // this.load.image('space', '/images/Background.png');
-  // this.load.image('alien', '/images/Aliens.png');
-  // this.load.image('door', '/images/ShipDoor.png');
-  // this.load.image('background', '/Starship-Map.png');
-  this.load.spritesheet("captain", "/images/YappinToTheCaptain.png", {
-    frameWidth: 80,
-    frameHeight: 80,
-  });
-  this.load.spritesheet("alien", "/images/Alien1.png", {
-    frameWidth: 80,
-    frameHeight: 80,
-  });
+        this.load.image("wall-tiles", "tilesets/Ship2_Bottom.png");
+        this.load.image("machinary1-tiles", "tilesets/SciFi_Computers_1.png");
+        this.load.image("machinary2-tiles", "tilesets/SciFi_Computers_2.png");
+        this.load.image("screens-tiles", "tilesets/!$ViewScreen_7.png");
+        this.load.image("door-tiles", "/tilesets/!$Objects_1.png");
+        this.load.tilemapTiledJSON("starship", "tilesets/Starship-Map.json");
+        // this.load.image('ship', '/images/Ship3_Bottom.png');
+        // this.load.image('space', '/images/Background.png');
+        // this.load.image('alien', '/images/Aliens.png');
+        // this.load.image('door', '/images/ShipDoor.png');
+        // this.load.image('background', '/Starship-Map.png');
+        this.load.spritesheet("captain", "/images/YappinToTheCaptain.png", {
+            frameWidth: 80,
+            frameHeight: 80,
+        });
+        this.load.spritesheet("alien", "/images/Alien1.png", {
+            frameWidth: 85,
+            frameHeight: 96.5,
+        });
     }
     
     create() {
@@ -72,17 +73,14 @@ class Scene1 extends Phaser.Scene {
         if (loadedPlayer) {
             let location = JSON.parse(loadedPlayer);
             player.sprite.setPosition(location.x, location.y)
+            let loadHealth= localStorage.getItem('healthBar');
+            let hBar = JSON.parse(loadHealth)
+            player.healthBar.value = hBar;
         }
 
-        this.physics.add.collider(
-            player.sprite,
-            doorLayer,
-            function () {
-              this.scene.start("endingScene");
-            },
-            null,
-            this
-          );
+        this.physics.add.collider(player.sprite, doorLayer, function () {
+            this.scene.start("endingScene"); }, null, this
+        );
     
         enemy = new Enemy(this, 800, 300);
         enemy.sprite.setImmovable(true)
@@ -94,35 +92,59 @@ class Scene1 extends Phaser.Scene {
         }
     
         this.physics.add.collider(player.sprite, enemy.sprite, function () {
-            player.sprite.setTint(0xff0000);
+            // player.sprite.setTint(0xff0000);
             const damageAmount = 0.5;
             player.healthBar.decrease(damageAmount);
-        });
-
-        platform = this.physics.add.staticGroup();
-        let door = platform.create(160, 0, "door").setAlpha(0);
-        this.physics.add.collider(
-            player.sprite,
-            door,
-            function () {
-            this.scene.start("Example2");
-            },
-            null,
-            this
-        );
+            if (player.healthBar.value === 0){
+                let gameOver = this.add.text(500,300, "Game Over", { fontFamily: "Comic Sans MS", font: "20px Impact", color: "black"})//, stroke: '#ffff00', strokeThickness: 2})
+                this.tweens.addCounter({
+                    from: 0,
+                    to: 1,
+                    duration: 3000,
+                    //yoyo: true,
+                    onUpdate: (tween) => {
+                        const v = tween.getValue();
+                        const c = 255 * v;
+                        gameOver.setFontSize(40 + v * 64);
+                        gameOver.setColor(`rgb(${c}, 0,0)`)//${c}, ${c})`);
+                    }
+                });
+                player.sprite.body.setEnable(false)//try 
+                player.sprite.disableBody(true,true)
+                // player.sprite.setTexture('deadPlayer').setScale(.50)
+                saveAndQuit.destroy()
+                const restartBtn = this.add.text(300,600, "Restart", { fontFamily: "Comic Sans MS", font: "30px Impact"})
+                restartBtn.setInteractive({cursor: 'pointer'}).on('pointerdown', () => 
+                    this.scene.start('Scene1')
+                )
+                // setTimeout(() => {
+                    // saveAndQuit.destroy()
+                    // const restartBtn = this.add.text(300,600, "Restart", { fontFamily: "Comic Sans MS", font: "30px Impact"})
+                    // restartBtn.setInteractive({cursor: 'pointer'}).on('pointerdown', () => 
+                    // this.scene.start('Scene1'))
+                //   }, 3100);
+                //this.scene.pause()//will not allow me to use buttons
+                //also at this point in time it would be ok for the player to go back to the main menu
+                //and continue from his last save point. so i keep the main menu button but try to 
+                //use set timout to destroy current text and replace with a restart button
+            }
+        }, null, this);
 
         const mainMenu = this.add.text(600,600, "Main Menu", { fontFamily: "Comic Sans MS", font: "30px Impact", })
-        mainMenu.setInteractive().on('pointerdown', () => { 
+        mainMenu.setInteractive({cursor: 'pointer'}).on('pointerdown', () => { 
             this.scene.start('MainMenu') 
         });
         
         const saveAndQuit = this.add.text(300,600, "Save & Quit", { fontFamily: "Comic Sans MS", font: "30px Impact", })
-        saveAndQuit.setInteractive().on('pointerdown', () => { 
+        saveAndQuit.setInteractive({cursor: 'pointer'}).on('pointerdown', () => { 
             localStorage.setItem('scene', this.scene.key)
             localStorage.setItem('player', JSON.stringify({ x: player.sprite.x, y: player.sprite.y}))
             localStorage.setItem('enemy', JSON.stringify({x: enemy.sprite.x, y: enemy.sprite.y}))
+            localStorage.setItem('healthBar', JSON.stringify(player.healthBar.value))//{x: player.healthBar.x, y: player.healthBar.y}))
+
             //SAVE PLAYER INVENTORY
             //WILL ITEMS COLLECTED REMAIN COLLECTED??
+            // console.log(player)
             this.scene.start('MainMenu') 
         });
     }
@@ -132,5 +154,6 @@ class Scene1 extends Phaser.Scene {
         enemy.update(player);
     }
 }
+
 
 module.exports = Scene1;
