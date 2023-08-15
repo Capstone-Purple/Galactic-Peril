400c0fdff5b549c321d666a727a0980b58799608
JoeLomuto <lomutoj@gmail.com>
2023-08-15 16:58:36 -0400

    working. still need to have messages appear at certain points in different scenes

--- src/Scene1.js
+++ src/Scene1.js
@@ -2,6 +2,7 @@ const Phaser = require("phaser");
 const Player = require("./player.js").default;
 const Enemy = require("./enemy.js").default;
 const placeMenus = require ("./boilerplate.js").default;
+const TextBox = require("./player.js").default;
 
 let player;
 let enemy;
@@ -240,7 +241,16 @@ class Scene1 extends Phaser.Scene {
   update() {
     player.update();
     enemy.update(player);
-  }
+
+  //   if (player.sprite.x > 500 && player.sprite.y > 300) {
+  //     const textBox = new TextBox(this, player.sprite.x, player.sprite.y - 50);
+  //     textBox.showText("Locked! The key has to be around here somewhere");
+      
+  //     setTimeout(() => {
+  //       textBox.hideText();
+  //     }, 5000);
+  // }
+}
 }
 
 module.exports = Scene1;
